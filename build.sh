8187f13008f06043d8827d0e7f8b632f61b3dd91
qkauia-guy <kauia407@gmail.com>
2026-01-16 02:10:44 +0800

    Recreate build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,3 @@
-# 建立檔案並寫入內容
-cat <<EOF > build.sh
 #!/usr/bin/env bash
 set -o errexit
 
@@ -7,6 +5,3 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 python manage.py createsuperuser --noinput || true
-EOF
-
-
